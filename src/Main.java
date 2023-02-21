fd984616a6c8b965d8e03868dcc4429711d43a87
KarlisKocins <karlis.kocins@edu.rtu.lv>
2023-02-21 16:25:13 +0200

    Fixed by requirements(byte array)

--- src/Main.java
+++ src/Main.java
@@ -25,34 +25,31 @@ public class Main {
 
     private static void comp(String str) {
     LinkedList<String> linkedArray = new LinkedList<>();
-    StringBuilder test = new StringBuilder();
+    byte[] bytes = new byte[(str.length() / 4) + 1];
     linkedArray.add(0, String.valueOf(str.length()));
-    int BitIndex = 0, ByteIndex = 1;
+    int BitIndex = 0, ByteIndex = 0;
         for (int i = 0; i < str.length(); i++) {
+            byte b = 0;
             switch (str.charAt(i)) {
-                case 'A' -> test.append("00");
-                case 'C' -> test.append("01");
-                case 'G' -> test.append("10");
-                case 'T' -> test.append("11");
+                case 'A' -> b = 0;
+                case 'C' -> b = 1;
+                case 'G' -> b = 2;
+                case 'T' -> b = 3;
             }
+            bytes[ByteIndex] |= (b << (6 - 2 * BitIndex));
             BitIndex++;
             if (BitIndex == 4){
-                linkedArray.add(ByteIndex, String.valueOf(test));
-                test.setLength(0); // reset StringBuilder
                 BitIndex = 0;
                 ByteIndex++;
             }
         }
         if (BitIndex != 0) { // add remaining bits to linkedArray
-            test.append("00".repeat(4 - BitIndex));
-            linkedArray.add(ByteIndex, String.valueOf(test));
+            bytes[ByteIndex] |= (0 << (6 - 2 * BitIndex));
         }
         // Convert binary strings to hexadecimal and put them back into linkedArray
-        for (int i = 1; i < linkedArray.size(); i++) {
-            String binary = linkedArray.get(i);
-            int decimal = Integer.parseInt(binary, 2);
-            String hexStr = Integer.toString(decimal, 16);
-            linkedArray.set(i, hexStr.toUpperCase());
+        for (byte b : bytes) {
+            String hexStr = String.format("%2X", b);
+            linkedArray.add(hexStr);
         }
         System.out.println(linkedArray);
     }
